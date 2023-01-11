623a9906b1c0cde9f5485385df2b5b865868a538
Yaswanth G <gyaswanth01@gmail.com>
2023-01-12 01:48:23 +0530

    testing changes deployment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-black'>
         <div className='max-w-screen-lg mx-auto flex flex-col  justify-center items-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
-                <h1 className='text-4xl sm:text-7xl font-bold text-white hover:text-cyan-400 duration-1000'>Hey, This is Yaswanth</h1>
+                <h1 className='text-4xl sm:text-7xl font-bold text-white hover:text-cyan-400 duration-1000'>Hey, This is Yaswanth Gajula</h1>
                 <p className='text-gray-500 py-4 max-w-md'>Currently there are more than 197,046,670 active websites in the World Wide Web. <br />Spending your time for visiting my portfolio webpage adds value to me and my skills. <br />Click the button below to get started. </p>
                 <div>
                     <Link to={"about"} smooth duration={1000} className='group flex text-white font-bold w-fit items-center rounded-md px-6 py-3 my-2  bg-gradient-to-r to-blue-500 from-cyan-500 cursor-pointer'>
