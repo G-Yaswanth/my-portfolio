370c3fcd8316561b2303d225b0a483394125d619
G-Yaswanth <76723625+G-Yaswanth@users.noreply.github.com>
2025-01-15 14:57:19 -0500

    Update About.jsx
    Made a change in my description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <div className='pb-8'>
                 <h2 className='text-4xl font-bold inline border-b-4 border-gray-500'>About</h2>
             </div>
-            <p className='text-xl mt-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui deserunt ab nulla praesentium quis repellendus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit nihil ab sint ipsam, nesciunt pariatur.</p>
+            <p className='text-xl mt-10'>An enthuiast with a dream to achieve big by integrating IT with real world problems.</p>
             <br />
             <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, asperiores. Eius nobis rerum repellat eos. Suscipit aut qui corrupti consectetur.</p>
         </div>
